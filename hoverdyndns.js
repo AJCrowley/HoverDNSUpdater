80578c7e23919f7015296cea2cdef6f4c6641475
Kris McCann <kris@8pi.ca>
2018-06-09 16:54:05 -0300

    Added support for new subdomains, updated readme

--- hoverdyndns.js
+++ hoverdyndns.js
@@ -6,9 +6,9 @@ const domain = process.argv[2];
 const subdomain = process.argv[3];
 
 // check that we've been passed a domain and subdomain
-if(domain == undefined || subdomain == undefined) {
+if(domain === undefined || subdomain === undefined) {
     // params not passed, provide usage notification to user
-    console.error('Usage: node index parentdomain.com subdomain');
+    console.error('Usage: node index [parent domain] [subdomain]');
 } else {
     // we have everything we need, get our IP address
     console.log('Checking external IP...');
@@ -24,21 +24,33 @@ if(domain == undefined || subdomain == undefined) {
         hover.getDomainDns(domain, (err, res) => {
             // filter results for our subdomain
             const entry = res[0].entries.filter((entry) => entry.name === subdomain)[0];
-            // check IP on A record vs our recent request, do they match?
-            if(entry.content == ip) {
-                // they do match, we don't need to do anything else
-                console.log(`IP unchanged from ${ip}, no update necessary...`);
-            } else {
-                // new IP, let the user know
-                console.log(`IP on record (${entry.content}) does not match current IP (${ip}), removing old A record...`);
-                // remove old A record
-                hover.removeDns(entry.id, () => {
-                    console.log('Creating new A record...');
-                    // create new A record with current IP
-                    hover.createARecord(domain, subdomain, ip, () => {
-                        // all done!
-                        console.log(`A record for ${subdomain}.${domain}: ${ip}`);
+            // check if there's an existing entry
+            if(entry !== undefined) {
+                if(entry.content === ip) {
+                    // they do match, we don't need to do anything else
+                    console.log(`IP unchanged from ${ip}, no update necessary...`);
+                    // exit
+                    return;
+                } else {
+                    // new IP, let the user know
+                    console.log(`IP on record (${entry.content}) does not match current IP (${ip}), removing old A record...`);
+                    // remove old A record
+                    hover.removeDns(entry.id, () => {
+                        console.log('Creating new A record...');
+                        // create new A record with current IP
+                        hover.createARecord(domain, subdomain, ip, () => {
+                            // all done!
+                            console.log(`A record for ${subdomain}.${domain}: ${ip}`);
+                        });
                     });
+                }
+            } else {
+                // this is a new A record
+                console.log(`No entry for domain ${subdomain}.${domain}, creating A record for ${ip}...`);
+                // create new A record with current IP
+                hover.createARecord(domain, subdomain, ip, () => {
+                    // all done!
+                    console.log(`A record for ${subdomain}.${domain}: ${ip}`);
                 });
             }
         });
