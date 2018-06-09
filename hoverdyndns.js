12726cdfdde21f6214a544a054d57c25dac54ef1
Kris McCann <kris@8pi.ca>
2018-06-09 00:17:59 -0300

    added comments

--- hoverdyndns.js
+++ hoverdyndns.js
@@ -17,15 +17,23 @@ if(domain == undefined || subdomain == undefined) {
         }
         // output result to console
         console.log(`Received result from ${service}: ${ip}...`);
+        // get list of domains from Hover
         hover.getDomainDns(domain, (ptr, res) => {
+            // filter results for our subdomain
             const entry = res[0].entries.filter((entry) => entry.name === subdomain)[0];
+            // check IP on A record vs our recent request, do they match?
             if(entry.content == ip) {
+                // they do match, we don't need to do anything else
                 console.log(`IP unchanged from ${ip}, no update necessary...`);
             } else {
+                // new IP, let the user know
                 console.log(`IP on record (${entry.content}) does not match current IP (${ip}), removing old A record...`);
+                // remove old A record
                 hover.removeDns(entry.id, () => {
                     console.log('Creating new A record...');
+                    // create new A record with current IP
                     hover.createARecord(domain, subdomain, ip, () => {
+                        // all done!
                         console.log(`A record for ${subdomain}.${domain}: ${ip}`);
                     });
                 });
